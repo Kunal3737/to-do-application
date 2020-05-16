2e5dae3111db0c234704481b7cbe22804c1d5998
Kunal Shinde <kunalshinde373@gmail.com>
2020-05-17 04:33:21 +0530

    Updates

---
+++ precache-manifest.b31ed8a81cf6b4b35fd1a7f58a9c3a39.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c88ad1d25cdb81186d1b4ec597a9f407",
+    "url": "/to-do-application/index.html"
+  },
+  {
+    "revision": "a511afdc0ec0b1399981",
+    "url": "/to-do-application/static/css/main.d07f17ae.chunk.css"
+  },
+  {
+    "revision": "cfb83c2f5ca9b685e97a",
+    "url": "/to-do-application/static/js/2.bc84e983.chunk.js"
+  },
+  {
+    "revision": "531d7b2065717e21d8dbdd1394bf24a4",
+    "url": "/to-do-application/static/js/2.bc84e983.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a511afdc0ec0b1399981",
+    "url": "/to-do-application/static/js/main.aa401f7d.chunk.js"
+  },
+  {
+    "revision": "cfe5f5725bd7dc90bedf",
+    "url": "/to-do-application/static/js/runtime-main.4b55a04a.js"
+  }
+]);
